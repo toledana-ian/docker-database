6213cddf9024a05ecb7ec3e7db02ca1b6d7c7d81
Christian Toledana <toledana.ian@gmail.com>
2022-09-13 11:11:48 +0800

    Remove useless docker config

--- docker/config/frontend/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp .env.docker .env
-npm install
-npm start
-#npm run build
-#npm install -g serve
-#serve -s build
--- docker/config/nodejs/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp .env.docker .env
-npm install
-npm run dev
-#npm start
